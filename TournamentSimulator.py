46d732396d3ca5266f0c2445d77da4af67e5e9d2
Stephen42Kim <stephen42kim@gmail.com>
2023-03-20 04:06:50 -0500

    New commit

---
+++ TournamentSimulator.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 03:52:15 2023
+
+@author: Stephen
+"""
+
+class TournamentSimulator:
+    '''Base class to create any kind of playoff simulator'''
+
+    def __init__(self):
+        pass
+
+
+    def SimulateMatchup(self):
+        '''Function to simulate one game'''
+        pass
